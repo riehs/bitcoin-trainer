118a8dddf15d2f0592b6677f54a3c380d5b971be
Daniel Riehs <riehs@bc.edu>
2016-04-02 23:38:13 -0400

    Add QR code to Receive Bitcoin page

--- Bitcoin Trainer/Bitcoin Trainer/ReceiveBitcoinViewController.swift
+++ Bitcoin Trainer/Bitcoin Trainer/ReceiveBitcoinViewController.swift
@@ -11,16 +11,40 @@ import UIKit
 class ReceiveBitcoinViewController: UIViewController {
 
     @IBOutlet weak var addressDisplay: UITextField!
+    @IBOutlet weak var imgQRCode: UIImageView!
 
     @IBAction func dimissReceiveBitcoin(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+	var qrCodeImage: CIImage!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		addressDisplay.text = BitcoinAddress.sharedInstance().address
+
+		//Convert the Bitcoin address to a QR code.
+		let data = addressDisplay.text!.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
+
+		let filter = CIFilter(name: "CIQRCodeGenerator")
+
+		filter!.setValue(data, forKey: "inputMessage")
+		
+		//The "Q" represents the level of error-correction data included in the QR code.
+		filter!.setValue("Q", forKey: "inputCorrectionLevel")
+
+		qrCodeImage = filter!.outputImage
+
+		//Determine appropriate scale factor for image.
+		let scaleX = imgQRCode.frame.size.width / qrCodeImage.extent.size.width
+		let scaleY = imgQRCode.frame.size.height / qrCodeImage.extent.size.height
+
+		//Apply scale factor to image.
+		let transformedImage = qrCodeImage.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
+
+		//Convert the Core Image to a UIImage.
+		imgQRCode.image = UIImage(CIImage: transformedImage)
 	}
 
 
